4fda94a37a982a1b3d0b4fb08c9647c8f6bf6396
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-21 16:23:56 +0000

    words instead of splitOn

--- HaskellFormatImport/Plugin.hs
+++ HaskellFormatImport/Plugin.hs
@@ -91,7 +91,7 @@ padAs n s =
         padDiff    = n - lenModName 
      in mconcat . intersperse (take padDiff (repeat ' ') ++ " as ") $ splitOn " as " $ removeRedundantWhitespace s
 
-removeRedundantWhitespace s = mconcat $ intersperse " " $ filter (/= "") $ splitOn " " s
+removeRedundantWhitespace = mconcat . intersperse " " . words
 
 sortImports :: [(LineNumber, ImportStatement)] -> [(LineNumber, ImportStatement)]
 sortImports xs = zip (fmap fst xs) $ sortBy (\a b -> compare (toLower <$> ignoreQualified (unImportStatement a)) (toLower <$> ignoreQualified (unImportStatement b))) (fmap snd xs)
